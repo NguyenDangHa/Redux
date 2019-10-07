9a567471853939349a36640251c1e06aa83432ef
NguyenDangHa <hanguyendang@vooc.vn>
2019-10-07 17:23:41 +0700

    24h

--- src/components/FootballScreen.js
+++ src/components/FootballScreen.js
@@ -5,7 +5,7 @@ import { WebView } from 'react-native';
 class FootballScreen extends Component {
     render() {
       return (
-        <WebView source={{ uri: 'https://www.24h.com.vn/bong-da-c48.html' }} />
+        <WebView source={{ uri: 'https://www.24h.com.vn' }} />
       );
     }
   }
